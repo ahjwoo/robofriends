ab94c61211e768e784909cb70cd125e6a0876050
Andrew Woo <ahjwoo@gmail.com>
2019-03-11 18:34:31 -0700

    robofriends

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Graham Toolbar',
+    username: 'Gray',
+    email: 'Graygray@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Gregory Apple',
+    username: 'Greg',
+    email: 'Gregoryapple@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Bud Kass',
+    username: 'Kassadin',
+    email: 'Kassss@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Lux Teemo',
+    username: 'Shroomie',
+    email: 'Poisonshrooms@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Blitz Crank',
+    username: 'Pull',
+    email: 'Ipullyou@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Garen Demacia',
+    username: 'Justice',
+    email: 'ForDemacia@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Caitlyn Vayne',
+    username: 'Trap',
+    email: 'Trapsfordays@gmail.com'
+  }
+]
